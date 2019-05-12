e543a1919738e4101b88324b6676f806f717034f
Tri-Waliangga <Waliangga@Gmail.com>
2019-05-12 19:46:13 +0700

    Updates

---
+++ precache-manifest.79d1517fbc1f669d4e1ced25e2b883be.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "0540dc6e306f3298454a",
+    "url": "/animal-sound/static/js/runtime~main.6baa54a7.js"
+  },
+  {
+    "revision": "49a9119c308823fbf968",
+    "url": "/animal-sound/static/js/main.a9eec147.chunk.js"
+  },
+  {
+    "revision": "9eee67cf782a38d35aeb",
+    "url": "/animal-sound/static/js/2.d6a534e5.chunk.js"
+  },
+  {
+    "revision": "49a9119c308823fbf968",
+    "url": "/animal-sound/static/css/main.4b22f56e.chunk.css"
+  },
+  {
+    "revision": "79ae5215a59d143fa691856b5765663b",
+    "url": "/animal-sound/index.html"
+  }
+];
